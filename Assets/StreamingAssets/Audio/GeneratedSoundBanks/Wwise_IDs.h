b65692160f3061decb45a492bd0e4ec4cef1bbcb
SuperMazy <supermazy91@gmail.com>
2021-11-23 14:23:39 +0000

    Project Rebuild (again)

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,42 +14,22 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID DOG_FOOTSTEPS = 4060911613U;
-<<<<<<< Updated upstream
-        static const AkUniqueID DOG_GROWL = 1201775887U;
-        static const AkUniqueID DOG_SNIFF = 127297048U;
         static const AkUniqueID FOX_FOOTSTEPS = 648125588U;
-        static const AkUniqueID PLAY_BREEZE = 3798124563U;
-        static const AkUniqueID PLAY_COLD_GUSTS = 400548325U;
-        static const AkUniqueID PLAY_SNEAK_LOOP = 4226239971U;
-        static const AkUniqueID PLAY_SNOW = 4026051667U;
-=======
-        static const AkUniqueID FOX_FOOTSTEPS = 648125588U;
-        static const AkUniqueID PLAY_BIRDS = 3025897706U;
-        static const AkUniqueID PLAY_BUSH = 1691497952U;
-        static const AkUniqueID PLAY_GUSTS = 3740536646U;
-        static const AkUniqueID PLAY_SNEAK_LOOP = 4226239971U;
-        static const AkUniqueID PLAY_WIND = 1020223172U;
->>>>>>> Stashed changes
+        static const AkUniqueID MUSIC_SNEAKING_PLAY = 3938542110U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-<<<<<<< Updated upstream
-        static const AkUniqueID ATMOSPHERE = 2145253833U;
         static const AkUniqueID DOGS = 2012068516U;
         static const AkUniqueID ENVIRONMENT = 1229948536U;
         static const AkUniqueID FOX = 1083137540U;
         static const AkUniqueID MUSIC = 3991942870U;
-=======
-        static const AkUniqueID SOUNDBANK = 1661994096U;
->>>>>>> Stashed changes
     } // namespace BANKS
 
     namespace BUSSES
     {
-        static const AkUniqueID ATMOSPHERE = 2145253833U;
-        static const AkUniqueID ENEMIES = 2242381963U;
+        static const AkUniqueID DOGS = 2012068516U;
         static const AkUniqueID ENVIRONMENT = 1229948536U;
         static const AkUniqueID FOX = 1083137540U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
